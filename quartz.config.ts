ba06566699df8951c06c0f2f54c7e1399e4c0ea6
Narcyy <65540093+weyway@users.noreply.github.com>
2025-02-25 05:11:08 +0000

    Quartz sync: Feb 25, 2025, 5:11 AM

---
+++ quartz.config.ts
@@ -0,0 +1,93 @@
+import { QuartzConfig } from "./quartz/cfg"
+import * as Plugin from "./quartz/plugins"
+
+/**
+ * Quartz 4.0 Configuration
+ *
+ * See https://quartz.jzhao.xyz/configuration for more information.
+ */
+const config: QuartzConfig = {
+  configuration: {
+    pageTitle: "💖 Weyway",
+    pageTitleSuffix: "",
+    enableSPA: true,
+    enablePopovers: true,
+    analytics: null,
+    locale: "en-US",
+    baseUrl: "https://weyway.github.io/",
+    ignorePatterns: ["private", "templates", ".obsidian"],
+    defaultDateType: "created",
+    generateSocialImages: false,
+    theme: {
+      fontOrigin: "googleFonts",
+      cdnCaching: true,
+      typography: {
+        header: "Playwrite IT Moderna",
+        body: "Comfortaa",
+        code: "IBM Plex Mono",
+      },
+      colors: {
+        lightMode: {
+          light: "#efe9d9",
+          lightgray: "#fffbe7",
+          gray: "#666666",
+          darkgray: "#4f3559",
+          dark: "#4f3559",
+          secondary: "#f38ba8",
+          tertiary: "#f38ba8",
+          highlight: "rgba(143, 159, 169, 0.15)",
+          textHighlight: "#fff23688",
+        },
+        darkMode: {
+          light: "#2d1a34",
+          lightgray: "#6f4080",
+          gray: "#646464",
+          darkgray: "#efe9d9",
+          dark: "#efe9d9",
+          secondary: "#f38ba8",
+          tertiary: "#84a59d",
+          highlight: "rgba(143, 159, 169, 0.15)",
+          textHighlight: "#b3aa0288",
+        },
+      },
+    },
+  },
+  plugins: {
+    transformers: [
+      Plugin.FrontMatter(),
+      Plugin.CreatedModifiedDate({
+        priority: ["frontmatter", "filesystem"],
+      }),
+      Plugin.SyntaxHighlighting({
+        theme: {
+          light: "github-light",
+          dark: "github-dark",
+        },
+        keepBackground: false,
+      }),
+      Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
+      Plugin.GitHubFlavoredMarkdown(),
+      Plugin.TableOfContents(),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.Description(),
+      Plugin.Latex({ renderEngine: "katex" }),
+    ],
+    filters: [Plugin.RemoveDrafts()],
+    emitters: [
+      Plugin.AliasRedirects(),
+      Plugin.ComponentResources(),
+      Plugin.ContentPage(),
+      Plugin.FolderPage(),
+      Plugin.TagPage(),
+      Plugin.ContentIndex({
+        enableSiteMap: true,
+        enableRSS: true,
+      }),
+      Plugin.Assets(),
+      Plugin.Static(),
+      Plugin.NotFoundPage(),
+    ],
+  },
+}
+
+export default config
