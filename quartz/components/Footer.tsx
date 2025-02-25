1b577893b22f890c79a6dceddce3896efd96e5c1
Narcyy <65540093+weyway@users.noreply.github.com>
2025-02-25 04:42:51 +0000

    update

---
+++ quartz/components/Footer.tsx
@@ -0,0 +1,33 @@
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import style from "./styles/footer.scss"
+import { version } from "../../package.json"
+import { i18n } from "../i18n"
+
+interface Options {
+  links: Record<string, string>
+}
+
+export default ((opts?: Options) => {
+  const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
+    const year = new Date().getFullYear()
+    const links = opts?.links ?? []
+    return (
+      <footer class={`${displayClass ?? ""}`}>
+        <p>
+          Created by{" "}
+          <a href="https://tomestone.gg/character/15308727/weylyn-mavourneen">W'eylyn Mavourneen</a> © {year}
+        </p>
+        <ul>
+          {Object.entries(links).map(([text, link]) => (
+            <li>
+              <a href={link}>{text}</a>
+            </li>
+          ))}
+        </ul>
+      </footer>
+    )
+  }
+
+  Footer.css = style
+  return Footer
+}) satisfies QuartzComponentConstructor
